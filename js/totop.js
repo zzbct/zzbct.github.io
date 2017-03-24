be4095e77e258e76515e244586013efd919f8137
zz bct <1668856072@qq.com>
2017-03-24 14:33:47 +0800

    Site updated: 2017-03-24 14:33:47

--- js/totop.js
+++ js/totop.js
@@ -4,21 +4,13 @@
 	var upperLimit = 1000;
 
 	// Our scroll link element
-	var scrollElem = document.getElementById('#totop');
+	var scrollElem = document.getElementById('totop');
 
 	// Scroll to top speed
 	var scrollSpeed = 5;
 
 	// Show and hide the scroll to top link based on scroll position
 	scrollElem.style.display = "none";
-	window.onscroll(function () {
-		var scrollTop = $(document).scrollTop();
-		if ( scrollTop > upperLimit ) {
-			$(scrollElem).stop().fadeTo(300, 1); // fade back in
-		}else{
-			$(scrollElem).stop().fadeTo(300, 0); // fade out
-		}
-	});
 	window.addEventListener("scroll",function(){
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		if ( scrollTop > upperLimit ) {
