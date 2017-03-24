a284bea36efa0b2b4a883cb865b1ce917b6d46fb
zz bct <1668856072@qq.com>
2017-03-24 14:31:42 +0800

    Site updated: 2017-03-24 14:31:41

--- js/totop.js
+++ js/totop.js
@@ -1,17 +1,17 @@
-(function($) {
+(function() {
 	// When to show the scroll link
 	// higher number = scroll link appears further down the page
 	var upperLimit = 1000;
 
 	// Our scroll link element
-	var scrollElem = $('#totop');
+	var scrollElem = document.getElementById('#totop');
 
 	// Scroll to top speed
-	var scrollSpeed = 500;
+	var scrollSpeed = 5;
 
 	// Show and hide the scroll to top link based on scroll position
-	scrollElem.hide();
-	$(window).scroll(function () {
+	scrollElem.style.display = "none";
+	window.onscroll(function () {
 		var scrollTop = $(document).scrollTop();
 		if ( scrollTop > upperLimit ) {
 			$(scrollElem).stop().fadeTo(300, 1); // fade back in
@@ -19,9 +19,21 @@
 			$(scrollElem).stop().fadeTo(300, 0); // fade out
 		}
 	});
+	window.addEventListener("scroll",function(){
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		if ( scrollTop > upperLimit ) {
+			scrollElem.style.display = "block"; // fade back in
+		}else{
+			scrollElem.style.display = "block"; // fade out
+		}
+	},false)
 
 	// Scroll to top animation on click
-	$(scrollElem).click(function(){
-		$('html, body').animate({scrollTop:0}, scrollSpeed); return false;
-	});
-})(jQuery);
+	scrollElem.addEventListener("click",function(){
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		if (scrollTop > 0) {
+			 window.requestAnimationFrame(arguments.callee);
+			 window.scrollTo (0,scrollTop - (scrollTop/scrollSpeed));
+        }
+	},false);
+})();
